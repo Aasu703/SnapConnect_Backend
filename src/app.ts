167ab582dfae2e6a0c9cc83380b8aaa4a6933afe
Aayush Subedi <smellylips508@gmail.com>
2026-06-04 14:34:08 +0545

    First commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// Basic server code
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+}
+);
